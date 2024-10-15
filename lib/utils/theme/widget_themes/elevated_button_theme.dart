828c387641b14755af43e5b62576dc1a1925a765
shahria <shahriasayad9@gmail.com>
2024-10-15 19:46:37 +0600

    bug fixed

--- lib/utils/theme/widget_themes/elevated_button_theme.dart
+++ lib/utils/theme/widget_themes/elevated_button_theme.dart
@@ -3,8 +3,8 @@ import '../../constants/colors.dart';
 import '../../constants/sizes.dart';
 
 /* -- Light & Dark Elevated Button Themes -- */
-class ElevatedButtonTheme {
-  ElevatedButtonTheme._(); //To avoid creating instances
+class TElevatedButtonTheme {
+  TElevatedButtonTheme._(); //To avoid creating instances
 
 
   /* -- Light Theme -- */
