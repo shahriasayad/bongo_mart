dc69634903475e056ea3a940183acd3ff474c66b
shahria <shahriasayad9@gmail.com>
2024-10-25 23:25:53 +0600

    utilities are added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
+import 'package:bongo_mart/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-
+import 'package:bongo_mart/app.dart';
 void main() {
   runApp(App());
 }
 
-class App  extends StatelessWidget {
-  const App ({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-
-      ),
-    );
-  }
-}
