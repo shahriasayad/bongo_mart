529cfb37de1a77b4ae0718dafa29341bb1f38a65
shahria <shahriasayad9@gmail.com>
2024-11-20 20:08:06 +0600

    appbar & cart icon done

--- lib/features/shop/screens/home.dart
+++ lib/features/shop/screens/home.dart
@@ -1,6 +1,11 @@
+import 'package:bongo_mart/common/widgets/appbar/appbar.dart';
 import 'package:bongo_mart/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:bongo_mart/common/widgets/products_cart/cart_counter_icon.dart';
+import 'package:bongo_mart/utils/constants/colors.dart';
+import 'package:bongo_mart/utils/constants/text_strings.dart';
 import 'package:bongo_mart/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,12 +13,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final isDesktop = TDeviceUtils.isDesktop(context);
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             PrimaryHeaderContainer(
-              child: Container()
+              child: Column(
+                children: [
+                  MyAppBar(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(TTexts.homeAppbarTitle,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .apply(color: TColors.grey)),
+                        Text(TTexts.homeAppbarSubTitle,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall!
+                                .apply(color: TColors.white)),
+                      ],
+                    ),
+                    actions: [
+                      CartCounterIcon(
+                        onPressed: () {},
+                        iconColor: TColors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
